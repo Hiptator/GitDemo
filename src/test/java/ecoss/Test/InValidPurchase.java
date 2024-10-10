de84074209a5c85f2de8047ab2fdc5ed3ee60833
loh <abdullahghazali18@gmail.com>
2024-10-10 16:35:51 +0800

    From US team

---
+++ src/test/java/ecoss/Test/InValidPurchase.java
@@ -0,0 +1,13 @@
+package ecoss.Test;
+
+import java.io.IOException;
+
+import ecoss.TestComponents.BasedTest;
+
+public class InValidPurchase extends BasedTest {
+	
+	public void invalidUserLogin() throws IOException {
+		
+	}
+
+}
